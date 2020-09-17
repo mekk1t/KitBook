c18f855a8250f36d90fe23764004f0d78821b789
mekk1t <61093487+mekk1t@users.noreply.github.com>
2020-09-17 21:53:08 +0300

    Types: CD operations

--- RecipesWebApp/CookbookApp/Models/Repositories/Interfaces/ITypeRepository.cs
+++ RecipesWebApp/CookbookApp/Models/Repositories/Interfaces/ITypeRepository.cs
@@ -5,12 +5,12 @@ namespace KK.Cookbook.Models.Repositories.Interfaces
     public interface ITypeRepository
     {
         public void AddNewRecipeType(string name);
-        public void AddNewCategoryType(string name);
+        public void AddNewCookingType(string name);
         public void AddNewDishType(string name);
         public void AddNewIngredientType(string name);
 
         public void RemoveRecipeType(string name);
-        public void RemoveCategoryType(string name);
+        public void RemoveCookingType(string name);
         public void RemoveDishType(string name);
         public void RemoveIngredientType(string name);
     }
