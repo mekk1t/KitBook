b852c3eb999a660d649b280f1ac2783de0196a31
mekk1t <kit.kalimov@gmail.com>
2020-10-29 21:53:34 +0300

    Create IRepository.cs

---
+++ RecipesWebApp/KitBook/Models/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KitBook.Models.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Create(T entity);
+        T Read(Guid id);
+        IEnumerable<T> ReadList();
+        void Update(T entity);
+        void Delete(Guid id);
+    }
+}
