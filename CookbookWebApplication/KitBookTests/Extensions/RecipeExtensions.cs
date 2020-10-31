794286c6f23e6c67b54feab6b9a50dbfe9ece848
mekk1t <kit.kalimov@gmail.com>
2020-10-31 17:13:53 +0300

    Расширения для тестов репозиториев

---
+++ CookbookWebApplication/KitBookTests/Extensions/RecipeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using KitBook.Models.Database.Entities;
+
+namespace KitBookTests.Extensions
+{
+    public static class RecipeExtensions
+    {
+        public static void HasTypes(this Recipe recipe)
+        {
+            recipe.RecipeType.Should().NotBeNull();
+            recipe.CookingType.Should().NotBeNull();
+            recipe.DishType.Should().NotBeNull();
+        }
+
+        public static void HasTypesIds(this Recipe recipe)
+        {
+            recipe.CookingTypeId.Should().NotBeEmpty();
+            recipe.DishTypeId.Should().NotBeEmpty();
+            recipe.CookingTypeId.Should().NotBeEmpty();
+        }
+    }
+}
