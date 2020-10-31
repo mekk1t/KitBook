e5bfc05b336b5cf13cf5b496f8f593e3363cd98e
mekk1t <kit.kalimov@gmail.com>
2020-10-31 23:05:25 +0300

    Созданы классы сервисов

---
+++ KitBook/Models/Services/RecipeService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using KitBook.Helpers.Extensions;
+using KitBook.Models.Database.Entities;
+using KitBook.Models.DTO;
+using KitBook.Models.Repositories.Interfaces;
+using KitBook.Models.Services.Interfaces;
+
+namespace KitBook.Models.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly IRepositoryAdvanced<Recipe> repository;
+
+        public RecipeService(IRepositoryAdvanced<Recipe> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void CreateNewRecipe(RecipeDto dto)
+        {
+            repository.Create(dto.AsEntity());
+        }
+
+        public void DeleteRecipeById(Guid id)
+        {
+            repository.Delete(id);
+        }
+
+        public RecipeDto GetRecipeById(Guid id)
+        {
+            return repository.ReadWithRelationships(id).AsDto();
+        }
+
+        public IEnumerable<RecipeDto> GetRecipes()
+        {
+            return repository.ReadWithRelationships().AsDtoEnumerable();
+        }
+
+        public void UpdateRecipe(RecipeDto dto)
+        {
+            repository.Update(dto.AsEntity());
+        }
+    }
+}
