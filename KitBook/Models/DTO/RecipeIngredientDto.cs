643fe1aa017cb1327cea41fe1bf340d37e6ebc52
mekk1t <kit.kalimov@gmail.com>
2020-11-01 17:42:04 +0300

    Модель для добавления ингредиентов в рецепт

---
+++ KitBook/Models/DTO/RecipeIngredientDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBook.Models.DTO
+{
+    public class RecipeIngredientDto
+    {
+        public Guid RecipeId { get; set; }
+        public Guid IngredientId { get; set; }
+        public string Name { get; set; }
+        public int? Amount { get; set; }
+        public int? G { get; set; }
+        public int? Ml { get; set; }
+        public bool IsOptional { get; set; }
+    }
+}
