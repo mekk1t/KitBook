93f230bf069efff7e57e49a50907b85934f7ebb2
mekk1t <kit.kalimov@gmail.com>
2020-11-04 23:21:45 +0300

    Наброски скрипта по добавлению элементов в форму

--- KitBook/wwwroot/js/site.js
+++ KitBook/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function appendStagesToForm() {
+    var stagesNumber = $("#stagesCount").val();
+    var stagesContainer = document.getElementById("stages");
+
+    for (var i = 0; i < stagesNumber; i++) {
+
+        var currentStage = "Stages".concat("[", i, "]");
+
+        var indexLabel = document.createElement("label");
+        indexLabel.innerHTML = indexLabel.innerHTML.concat("№", i + 1);
+        var indexInput = document.createElement("input");
+        indexInput.setAttribute("readonly", "readonly");
+        indexInput.setAttribute("value", i);
+        var indexName = currentStage.concat(".Index");
+        indexInput.setAttribute("name", indexName);
+
+        var descriptionLabel = document.createElement("label");
+        descriptionLabel.innerHTML = "Описание шага";
+
+        var descriptionTextarea = document.createElement("textarea");
+        descriptionTextarea.setAttribute("name", currentStage.concat(".Description"));
+
+        stagesContainer.appendChild(indexLabel);
+        stagesContainer.appendChild(indexInput);
+        stagesContainer.appendChild(descriptionLabel);
+        stagesContainer.appendChild(descriptionTextarea);
+    }
+}
