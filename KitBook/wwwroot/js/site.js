585c905344620b5a02e56cd0d4faeec16eb43cde
mekk1t <kit.kalimov@gmail.com>
2020-11-06 22:06:03 +0300

    добавлен скрипт на +1 шаг

--- KitBook/wwwroot/js/site.js
+++ KitBook/wwwroot/js/site.js
@@ -4,7 +4,7 @@
 // Write your JavaScript code.
 function appendStagesToForm() {
     var stagesNumber = $("#stagesCount").val();
-    var stagesContainer = $("#stages");
+    let stagesContainer = $("#stages");
 
     var label = document.createElement("label");
     label.setAttribute("class", "control-label");
@@ -13,43 +13,56 @@ function appendStagesToForm() {
     stagesContainer.append(label);
 
     for (var i = 0; i < stagesNumber; i++) {
-
-        var currentStage = "Stages".concat("[", i, "]");
-
-        var stageContainer = document.createElement("div");
-        stageContainer.setAttribute("id", "stage_".concat(i));
-
-        var indexLabel = document.createElement("label");
-        indexLabel.innerHTML = indexLabel.innerHTML.concat("№", i + 1);
-        indexLabel.setAttribute("class", "control-label");
-        indexLabel.setAttribute("for", currentStage.concat("__Index"));
-
-        var indexInput = document.createElement("input");
-        indexInput.setAttribute("value", i + 1);
-        indexInput.setAttribute("class", "form-control");
-        indexInput.setAttribute("class", "invisible");
-        indexInput.setAttribute("type", "number");
-        indexInput.setAttribute("name", currentStage.concat(".Index"));
-
-        var descriptionLabel = document.createElement("label");
-        descriptionLabel.innerHTML = "Описание";
-        descriptionLabel.setAttribute("class", "control-label");
-
-        var descriptionTextarea = document.createElement("textarea");
-        descriptionTextarea.setAttribute("name", currentStage.concat(".Description"));
-        descriptionTextarea.setAttribute("class", "form-control");
-
-        var recipeIdInput = document.createElement("input");
-        recipeIdInput.setAttribute("class", "invisible");
-        recipeIdInput.setAttribute("name", currentStage.concat(".RecipeId"));
-        recipeIdInput.setAttribute("value", $("#Id").val())
-
-        stageContainer.append(indexLabel);
-        stageContainer.append(indexInput);
-        stageContainer.append(descriptionLabel);
-        stageContainer.append(descriptionTextarea);
-        stageContainer.append(recipeIdInput);
-
-        stagesContainer.append(stageContainer);
+        appendStageToContainer(i, stagesContainer);
     }
+    $("#stageInit").hide();
+    $("#MORE").show();
+}
+
+function moreStages() {
+    let stagesContainer = $("#stages");
+    appendStageToContainer(stagesContainer.children(".count").length, stagesContainer);
+}
+
+function appendStageToContainer(i, container) {
+
+    var currentStage = "Stages".concat("[", i, "]");
+
+    var stageContainer = document.createElement("div");
+    stageContainer.setAttribute("id", "stage_".concat(i));
+    stageContainer.setAttribute("class", "count");
+
+    var indexLabel = document.createElement("label");
+    indexLabel.innerHTML = indexLabel.innerHTML.concat("№", i + 1);
+    indexLabel.setAttribute("class", "control-label");
+    indexLabel.setAttribute("for", currentStage.concat("__Index"));
+
+    var indexInput = document.createElement("input");
+    indexInput.setAttribute("value", i + 1);
+    indexInput.setAttribute("class", "form-control");
+    indexInput.setAttribute("class", "invisible");
+    indexInput.setAttribute("type", "number");
+    indexInput.setAttribute("name", currentStage.concat(".Index"));
+
+    var descriptionLabel = document.createElement("label");
+    descriptionLabel.innerHTML = "Описание";
+    descriptionLabel.setAttribute("class", "control-label");
+
+    var descriptionTextarea = document.createElement("textarea");
+    descriptionTextarea.setAttribute("name", currentStage.concat(".Description"));
+    descriptionTextarea.setAttribute("class", "form-control");
+
+    var recipeIdInput = document.createElement("input");
+    recipeIdInput.setAttribute("class", "invisible");
+    recipeIdInput.setAttribute("name", currentStage.concat(".RecipeId"));
+    recipeIdInput.setAttribute("value", $("#Id").val())
+
+
+    stageContainer.append(indexLabel);
+    stageContainer.append(indexInput);
+    stageContainer.append(descriptionLabel);
+    stageContainer.append(descriptionTextarea);
+    stageContainer.append(recipeIdInput);
+
+    container.append(stageContainer);
 }
