0f8384e8f9eec5ad866bee08ad240dba30aef9c9
mekk1t <kit.kalimov@gmail.com>
2020-11-13 19:59:55 +0300

    Внедрение категорий для рецептов

---
+++ KitBook/Models/Database/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace KitBook.Models.Database.Entities
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<RecipeCategory> Recipes { get; set; }
+    }
+}
