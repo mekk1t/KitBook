4eda1ac0981194137f4c1c12066cf9cee8c0fdd8
mekk1t <kit.kalimov@gmail.com>
2020-11-22 11:53:32 +0300

    БД-фабрика для создания в другом проекте

---
+++ Dal/Database/Factory/DbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using KitBook.Models.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Dal.Database.Factory
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<CookbookDbContext>
+    {
+        private const string SQL_CONNECTION_STRING = "Server=localhost;Database=CookbookDB;Trusted_Connection=True;";
+
+        public CookbookDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CookbookDbContext>();
+            optionsBuilder.UseSqlServer(SQL_CONNECTION_STRING);
+
+            return new CookbookDbContext(optionsBuilder.Options);
+        }
+    }
+}
