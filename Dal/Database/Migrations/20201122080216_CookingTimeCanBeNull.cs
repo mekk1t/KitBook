8aecb7d12154157def5516a86ef27f7f3e2b1e09
mekk1t <kit.kalimov@gmail.com>
2020-11-22 11:53:10 +0300

    Лил правки сущностей

---
+++ Dal/Database/Migrations/20201122080216_CookingTimeCanBeNull.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KitBook.Models.Database.Migrations
+{
+    public partial class CookingTimeCanBeNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CookingTimeMinutes",
+                table: "Recipes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CookingTimeMinutes",
+                table: "Recipes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
