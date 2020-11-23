18f2aca331652ebd82127963ee4d2df447a7514f
mekk1t <kit.kalimov@gmail.com>
2020-11-24 00:25:49 +0300

    Модель типа для редактирования типов/категорий

---
+++ KitBook/Models/ViewModels/TypeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace KitBook.Models.ViewModels
+{
+    public class TypeViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IFormFile Icon { get; set; }
+    }
+}
