710148c68a535656d1472c9805bfbb032d4a0b46
mekk1t <kit.kalimov@gmail.com>
2020-11-24 00:28:02 +0300

    Интерфейс + реализация маппера моделей типов

---
+++ KitBook/Mappers/Interfaces/ITypeMapper.cs
@@ -0,0 +1,11 @@
+﻿using BusinessLogic.Models.Types.Interface;
+using KitBook.Models.ViewModels;
+
+namespace KitBook.Mappers.Interfaces
+{
+    public interface ITypeMapper
+    {
+        T Map<T>(TypeViewModel viewModel)
+            where T : IType, new();
+    }
+}
---
+++ KitBook/Mappers/TypeMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BusinessLogic.Models.Types.Interface;
+using KitBook.Handlers.Interface;
+using KitBook.Mappers.Interfaces;
+using KitBook.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+
+namespace KitBook.Mappers
+{
+    public class TypeMapper : ITypeMapper
+    {
+        private readonly IFileHandler<IFormFile> fileHandler;
+
+        public TypeMapper(IFileHandler<IFormFile> fileHandler)
+        {
+            this.fileHandler = fileHandler;
+        }
+
+        public T Map<T>(TypeViewModel viewModel)
+            where T:IType, new()
+        {
+            return new T
+            {
+                Id = viewModel.Id == Guid.Empty
+                ? Guid.NewGuid()
+                : viewModel.Id,
+                Name = viewModel.Name,
+                Icon = new BusinessLogic.Models.Files.File
+                {
+                    Extension = fileHandler.GetExtension(viewModel.Icon),
+                    Content = fileHandler.GetBytes(viewModel.Icon)
+                }
+            };
+        }
+    }
+}
