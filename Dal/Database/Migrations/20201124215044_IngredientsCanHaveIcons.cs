f138c8a04a2ef4411c8a96b882e74de421198428
mekk1t <kit.kalimov@gmail.com>
2020-11-25 01:27:45 +0300

    У ингредиентов теперь будут иконки

---
+++ Dal/Database/Migrations/20201124215044_IngredientsCanHaveIcons.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KitBook.Models.Database.Migrations
+{
+    public partial class IngredientsCanHaveIcons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "IconId",
+                table: "IngredientTypes",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientTypes_IconId",
+                table: "IngredientTypes",
+                column: "IconId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IngredientTypes_Files_IconId",
+                table: "IngredientTypes",
+                column: "IconId",
+                principalTable: "Files",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IngredientTypes_Files_IconId",
+                table: "IngredientTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_IngredientTypes_IconId",
+                table: "IngredientTypes");
+
+            migrationBuilder.DropColumn(
+                name: "IconId",
+                table: "IngredientTypes");
+        }
+    }
+}
