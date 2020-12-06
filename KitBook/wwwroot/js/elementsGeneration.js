ebf6484948e827ae34bca8ff2f990bbf681c7008
mekk1t <kit.kalimov@gmail.com>
2020-12-06 21:04:25 +0300

    Набросок кастомного набора сокращеннных операций

---
+++ KitBook/wwwroot/js/elementsGeneration.js
@@ -0,0 +1,31 @@
+﻿
+
+/**
+ * Sets the "id" attribute of the element.
+ * @param {HTMLElement} element
+ * @param {string} value
+ */
+function _id(element, value) {
+    element.setAttribute("id", value);
+}
+
+/**
+ * Sets the "class" attribute of the element.
+ * @param {HTMLElement} element
+ * @param {string} value
+ */
+function _class(element, value) {
+    element.setAttribute("class", value);
+}
+
+/**
+ * Sets the "value" attribute of the element.
+ * @param {HTMLElement} element
+ * @param {string} value
+ */
+function _value(element, value) {
+    element.setAttribute("value", value);
+}
+
+function blabla() {
+}
