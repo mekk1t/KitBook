1f71ad40cdfe06af2c52bbd43d3a2d79eeeeaa5e
mekk1t <kit.kalimov@gmail.com>
2020-12-10 23:43:23 +0300

    Update site.js

--- KitBook/wwwroot/js/site.js
+++ KitBook/wwwroot/js/site.js
@@ -9,11 +9,6 @@ afterLoading(() => {
 
 /** Applies "autosize" function to all textarea elements on the page. */
 function autosizeTextareas() {
-    var descriptionElement = document.getElementById("Description");
-    if (descriptionElement) {
-        autosize(descriptionElement);
-    }
-
     var textAreaElements = document.getElementsByClassName("custom-textarea")
     if (textAreaElements) {
         for (var i = 0; i < textAreaElements.length; i++) {
