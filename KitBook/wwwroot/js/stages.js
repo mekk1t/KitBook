6d84a07f16ea0e8c0a5a33a9096a4464a0e11fe5
mekk1t <kit.kalimov@gmail.com>
2020-12-10 23:43:35 +0300

    Рефакторинг скриптов по этапам, ч.1

--- KitBook/wwwroot/js/stages.js
+++ KitBook/wwwroot/js/stages.js
@@ -1,66 +1,76 @@
-﻿
+﻿/**
+ * Adds a collection of elements to the form to create a new Stage Input.
+ * */
+function moreStages() {
+    let stagesContainer = document.getElementById("stages");
+    let stagesCount = stagesContainer.childElementCount;
 
-function initLabel(stagesContainer) {
+    if (stagesCount == 0) {
+        initStagesLabel(stagesContainer);
+    }
+
+    appendStageToContainer(stagesCount, stagesContainer);
+}
+
+/**
+ * Creates a Stages label element and adds it to the provided container.
+ * @param {HTMLDivElement} container
+ */
+function initStagesLabel(container) {
     var label = document.createElement("label");
     label.setAttribute("class", "control-label");
     label.setAttribute("for", "Stages");
-    label.innerHTML = "Шаги по приготовлению";
+    label.innerHTML = "<strong>Шаги по приготовлению</strong>";
 
-    _class(label, "control-label");
-    $(stagesContainer).append(label);
+    container.appendChild(label);
 }
 
-function moreStages() {
-    let stagesContainer = $("#stages");
-    if (stagesContainer.children().length == 0) {
-        initLabel(stagesContainer);
-    }
-    appendStageToContainer(stagesContainer.children(".stagesDiv").length, stagesContainer);
-}
-
+/**
+ *
+ * @param {number} i Index of the stage to append.
+ * @param {HTMLDivElement} container Container to append the stage elements to.
+ */
 function appendStageToContainer(i, container) {
 
-    var currentStage = "Stages".concat("[", i, "]");
+    let elements = new Array();
 
-    var stageContainer = document.createElement("div");
+    const current = `Stages[${i}]`;
 
-    stageContainer.setAttribute("id", "stage_".concat(i));
-    stageContainer.setAttribute("class", "count");
+    let stageContainer = document.createElement("div");
+    stageContainer.setAttribute("id", `stages_${i}`);
+    stageContainer.setAttribute("class", "stage");
 
-    var indexLabel = document.createElement("label");
-    indexLabel.innerHTML = indexLabel.innerHTML.concat("Шаг № ", i + 1);
+    let indexLabel = document.createElement("label");
+    indexLabel.innerHTML = `Шаг №${i}`;
     indexLabel.setAttribute("class", "control-label");
-    indexLabel.setAttribute("for", currentStage.concat("__Index"));
 
-    var indexInput = document.createElement("input");
+    let indexInput = document.createElement("input");
     indexInput.setAttribute("value", i + 1);
-    indexInput.setAttribute("class", "form-control");
-    indexInput.setAttribute("class", "invisible");
+    indexInput.setAttribute("class", "form-control invisible");
     indexInput.setAttribute("type", "number");
-    indexInput.setAttribute("name", currentStage.concat(".Index"));
+    indexInput.setAttribute("name", current.concat(".Index"));
 
-    var descriptionLabel = document.createElement("label");
+    let descriptionLabel = document.createElement("label");
     descriptionLabel.innerHTML = "Описание";
     descriptionLabel.setAttribute("class", "control-label");
 
-    var descriptionTextarea = document.createElement("textarea");
-    descriptionTextarea.setAttribute("name", currentStage.concat(".Description"));
-    descriptionTextarea.setAttribute("class", "form-control");
-    autosize(descriptionTextarea);
+    let descriptionTextarea = document.createElement("textarea");
+    descriptionTextarea.setAttribute("name", current.concat(".Description"));
+    descriptionTextarea.setAttribute("class", "form-control lead");
 
-    var imageLabel = document.createElement("label");
+    let imageLabel = document.createElement("label");
     imageLabel.innerHTML = "Картинка";
     imageLabel.setAttribute("class", "control-label");
-    imageLabel.setAttribute("for", currentStage.concat(".Image"));
+    imageLabel.setAttribute("for", current.concat(".Image"));
 
-    var imageInput = document.createElement("input");
+    let imageInput = document.createElement("input");
     imageInput.setAttribute("type", "file");
     imageInput.setAttribute("class", "form-control-file");
-    imageInput.setAttribute("name", currentStage.concat(".Image"));
+    imageInput.setAttribute("name", current.concat(".Image"));
 
-    var recipeId = document.createElement("input");
+    let recipeId = document.createElement("input");
     recipeId.setAttribute("class", "invisible");
-    recipeId.setAttribute("name", currentStage.concat(".RecipeId"));
+    recipeId.setAttribute("name", current.concat(".RecipeId"));
     recipeId.setAttribute("value", $("#Id").val())
 
 
